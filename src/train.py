aaaed659c3d3c062371d1e5399703b0fe5299957
jose-mota4 <jose.mota4@vodafone.com>
2022-10-12 16:08:16 +0100

    add files for medium post

---
+++ src/train.py
@@ -0,0 +1,19 @@
+import logging
+from time import time
+
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+
+
+start_time = time()
+X, y = make_classification(n_samples=1000000, n_features=100, n_classes=2)
+
+lr_clf = LogisticRegression()
+
+lr_clf.fit(X, y)
+
+end_time = time()
+
+print(f"Training time: {end_time-start_time}")
+
+logging.info(f"Training time: {end_time-start_time}")
